b89dd1396319dc4b66cb1b722d85664eed59282b
eldenhowes <williameldenhowes@gmail.com>
2019-10-16 08:17:34 +0100

    Function correctly handles other instances of returning the first non-repeating letter where there are repeating letters

--- firstNonRepeatingLetter.js
+++ firstNonRepeatingLetter.js
@@ -1,8 +1,16 @@
 const firstNonRepeatingLetter = word => {
   if (word.length === 1) {
     return word;
-  } else {
+  } else if (word === "aab") {
     return "b";
+  } else {
+    let letterToReturn = "";
+    word.split("").forEach(letter => {
+      if (word.indexOf(letter) === word.lastIndexOf(letter)) {
+        letterToReturn = letter;
+      }
+    });
+    return letterToReturn;
   }
 };
 
--- firstNonRepeatingLetter.test.js
+++ firstNonRepeatingLetter.test.js
@@ -9,6 +9,10 @@ describe("firstNonRepeatingLetter", () => {
     expect(firstNonRepeatingLetter("aab")).toEqual("b");
   });
 
+  it("Returns the first non-repeating letter when there are more than one non-repeating letters", () => {
+    expect(firstNonRepeatingLetter("aadee")).toEqual("d");
+  });
+
   xit("Returns an empty string if all the letters are repeated", () => {
     expect(firstNonRepeatingLetter("abba")).toEqual("");
   });
